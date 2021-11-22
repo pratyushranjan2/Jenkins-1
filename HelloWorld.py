efaf0df74b6ea656d0fe804f16673fb44b60adeb
Pratyush Ranjan <51630699+pratyushranjan2@users.noreply.github.com>
2021-11-22 19:27:35 +0530

    Update HelloWorld.py

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,8 +1,8 @@
 #! /usr/bin/python3
 # This Phython program will print Hellow World...
-print("Hello World Demo...\n")
-print("Hello World Demo...\n")
-print("Hello World Demo...\n")
+print("Hello Jenkins Demo...\n")
+print("Hello Pipeline Demo...\n")
+print("Hello CICD Demo...\n")
 print("Hello World Demo...\n")
 print("Hello World Demo...\n")
 
