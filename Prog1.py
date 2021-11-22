7dc53fb391eb8d77bdf445421cbd3d3a389d332b
* <*pratyushranjan02@gmail.com*>
2021-11-22 20:59:44 +0530

    Jenkins Pipeline Project

--- Prog1.py
+++ Prog1.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
 # Source code for summation of two numbers
 
-def summation(data):
-    return sum(data)
-
 def factorial(n):
     if n < 0:
-        return null
+        return None
     f = 1
     for i in range(1,n+1):
         f *= i
---
+++ Test1.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+# Test case for adding two numbers
+import unittest
+
+from Prog1 import factorial
+
+class TestFactorialValid(unittest.TestCase):
+    def test_list_int(self):
+        """
+        Test case to get factorial of a valid number
+        """
+        data = 4
+        result = factorial(data)
+        self.assertEqual(result, 24)
+     
+if __name__ == '__main__':
+    unittest.main()
