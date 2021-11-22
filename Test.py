857221afab0331f33041e34480910bfe7d806c08
Pratyush Ranjan <51630699+pratyushranjan2@users.noreply.github.com>
2021-11-22 20:52:37 +0530

    Added testcases for factorial

--- Test.py
+++ Test.py
@@ -2,7 +2,7 @@
 # Test case for adding two numbers
 import unittest
 
-from Prog1 import summation
+from Prog1 import summation, factorial
 
 class TestSum(unittest.TestCase):
     def test_list_int(self):
@@ -12,6 +12,24 @@ class TestSum(unittest.TestCase):
         data = [23, 32]
         result = summation(data)
         self.assertEqual(result, 55)
+
+class TestFactorialValid(unittest.TestCase):
+    def test_list_int(self):
+        """
+        Test case to get factorial of a valid number
+        """
+        data = 4
+        result = factorial(data)
+        self.assertEqual(result, 24)
+     
+class TestFactorialInvalid(unittest.TestCase):
+    def test_list_int(self):
+        """
+        Test case to get factorial of an invalid number
+        """
+        data = -2
+        result = factorial(data)
+        self.assertEqual(result, null)
 
 if __name__ == '__main__':
     unittest.main()
